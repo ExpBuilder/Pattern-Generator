9ca51ee28ab954b861f47184f2a8a566a68d563b
ExpBuilder <102679788+ExpBuilder@users.noreply.github.com>
2025-10-29 11:04:37 -0400

    Create Patterns.java
    Basic set up for methods

---
+++ Patterns.java
@@ -0,0 +1,40 @@
+//
+//  Class author: Joe Jiao
+//  Date created: 10/29/2025
+//  General description:
+//
+public class Patterns {
+  public static void main(String[] args){
+    
+  }
+
+  //  Pre-condition: what must be true prior to the method being called including possible limitations on input
+  //  Post-condition: what the result of executing this method will be.
+  public static void stars(int rows){
+    
+  }
+
+  //  Pre-condition: what must be true prior to the method being called including possible limitations on input
+  //  Post-condition: what the result of executing this method will be.
+  public static void triangle(int rows){
+    
+  }
+
+  //  Pre-condition: what must be true prior to the method being called including possible limitations on input
+  //  Post-condition: what the result of executing this method will be.
+  public static void odds(int start){
+    
+  }
+  
+  //  Pre-condition: what must be true prior to the method being called including possible limitations on input
+  //  Post-condition: what the result of executing this method will be.
+  public static void eo(int maxE){
+    
+  }
+
+  //  Pre-condition: what must be true prior to the method being called including possible limitations on input
+  //  Post-condition: what the result of executing this method will be.
+  public static void pyramid(int rows){
+    
+  }
+}
