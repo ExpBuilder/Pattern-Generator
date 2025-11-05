3567c154e728115b4e8009cd411a9a5ccc4589b2
ExpBuilder <102679788+ExpBuilder@users.noreply.github.com>
2025-11-05 22:19:41 +0000

    Updated pre- and post-conditions

--- Patterns.java
+++ Patterns.java
@@ -16,9 +16,15 @@ public class Patterns {
     pyramid (5);
   }
 
-  //  Pre-condition: 
-  //  Post-condition: 
+  //  Pre-condition: Input rows must be a positive integer
+  /*  Post-condition: A right triangle or 'pyramid' with the vertical leg aligned to the left and horizontal leg aligned to the bottom is formed out of asterisks (*)
+
+  The pyramid is composed of layers increasing in size as one goes down the pyramid, with each layer being two astericks thicker than the previous. 
+  The first layer is formed of a single asterisk.
+  */
   public static void stars(int rows){
+    if (rows < 1) return;
+
     int row = 0;
     while (row < rows) {
       String placeholder = "";
@@ -33,9 +39,15 @@ public class Patterns {
     }
   }
 
-  //  Pre-condition: 
-  //  Post-condition: 
+  //  Pre-condition: Input rows must be a positive integer less than ten
+  /*  Post-condition:  A right triangle or 'pyramid' with the vertical leg aligned to the left and horizontal leg aligned to the bottom is formed out of numbers.
+
+  The numbers in each row correspond with their respective row number. For example, row 1 is made of 1s, row 2 is made of 2s, etc.
+  The length of each row is their respective row number. For example, row 1 is length 1.
+  */
   public static void triangle(int rows){
+    if (rows >= 10 || rows < 1) return;
+
     int row = 1;
     while (row <= rows) {
       String placeholder = "";
@@ -51,9 +63,13 @@ public class Patterns {
     }
   }
 
-  //  Pre-condition: 
-  //  Post-condition: 
+  //  Pre-condition: Input start must be an odd integer between 1 and 9, inclusive.
+  /*Post-condition: 
+
+  */
   public static void odds(int start){
+    if (start % 2 == 0 || start >= 10 || start < 1) return;
+
     for (int i = start; i > 0;) {
       String placeholder = "";
       for (int j = 0; j < i; j++) placeholder += i;
@@ -62,9 +78,11 @@ public class Patterns {
     }
   }
   
-  //  Pre-condition: 
+  //  Pre-condition: Input maxE must be a positive integer.
   //  Post-condition: 
   public static void eo(int maxE){
+    if (maxE < 1) return;
+
     for (int i = 0; i < maxE; i++) {
       char letter = 'O';
       if ((maxE + i) % 2 == 1) letter = 'E';
